6039851763d4bb4bc85dc650fcaf7384043bc5bb
quantumsheep <nathanael.dmc@outlook.fr>
2020-04-01 14:11:28 +0200

    feat(layout): Add JavaFX FXML file

--- src/main/java/com/tatakae/admin/App.java
+++ src/main/java/com/tatakae/admin/App.java
@@ -1,21 +1,32 @@
 package com.tatakae.admin;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.nio.file.Paths;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        try {
+            String userDirectory = Paths.get("")
+                    .toAbsolutePath()
+                    .toString();
+
+            System.out.println(userDirectory);
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+            Parent root = loader.load();
+
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
