c0c4d9e9f5a117d76311ab83cc585e9b256968c3
NenetteGit <antonin.texier@gmail.com>
2020-05-14 14:57:42 +0200

    feat(cli): Adapt scenario to refactor.

--- cli/src/main/java/com/tatakae/admin/cli/Application.java
+++ cli/src/main/java/com/tatakae/admin/cli/Application.java
@@ -1,16 +1,23 @@
 package com.tatakae.admin.cli;
 
-import com.tatakae.admin.core.Auth;
+import com.tatakae.admin.core.services.AuthService;
 import com.tatakae.admin.core.User;
 
 class Application {
 
     public static void main(final String[] args) {
-        var user = new User();
+        int counter = 5;
         do {
             System.out.println("Enter your credentials:\n");
-            user = user.login();
-        } while (!Auth.authed);
+            User.login();
+
+            if (AuthService.isAuthed()) {
+                counter--;
+                System.out.println("\nConnection failed. " + counter + " attempts remaining.\n");
+            } else {
+                System.out.println("\nConnection successful.\n");
+            }
+        } while (!AuthService.isAuthed() && counter > 0 );
 
         System.exit(0);
     }
