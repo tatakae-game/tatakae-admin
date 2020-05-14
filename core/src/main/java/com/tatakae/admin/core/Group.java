b16bed060f250e22da45d1e406d983d3631af7f3
NenetteGit <antonin.texier@gmail.com>
2020-05-14 14:56:16 +0200

    feat(core): Add entities for Group and Permission.

---
+++ core/src/main/java/com/tatakae/admin/core/Group.java
@@ -0,0 +1,42 @@
+package com.tatakae.admin.core;
+
+import java.util.ArrayList;
+
+public class Group {
+    private final String id;
+    private final String name;
+    private final ArrayList<Permission> permissions;
+
+    public Group() {
+        this.id = "";
+        this.name = "";
+        this.permissions = null;
+    }
+
+    public Group(final String id, final String name, final ArrayList<Permission> permissions) {
+        this.id = id;
+        this.name = name;
+        this.permissions = permissions;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Permission> getPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public String toString() {
+        return '{' +
+                "id: '" + id + '\'' +
+                ", name: '" + name + '\'' +
+                ", permissions: " + permissions +
+                '}';
+    }
+}
