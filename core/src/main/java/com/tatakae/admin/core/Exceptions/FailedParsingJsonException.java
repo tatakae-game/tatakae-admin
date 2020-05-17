381d84547c13131b4aed146ffd21672604fd0480
quantumsheep <nathanael.dmc@outlook.fr>
2020-05-17 21:12:25 +0200

    feat: Remove serial warning on FailedParsingJsonException

--- core/src/main/java/com/tatakae/admin/core/Exceptions/FailedParsingJsonException.java
+++ core/src/main/java/com/tatakae/admin/core/Exceptions/FailedParsingJsonException.java
@@ -1,5 +1,6 @@
 package com.tatakae.admin.core.Exceptions;
 
+@SuppressWarnings("serial")
 public class FailedParsingJsonException extends Exception {
     public FailedParsingJsonException(String message) {
         super(message);
