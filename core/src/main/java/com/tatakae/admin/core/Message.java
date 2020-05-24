5d7470fc386177864e2ebf51685aa2447fee0b45
NenetteGit <antonin.texier@gmail.com>
2020-05-24 16:07:31 +0200

    feat(core): Create room and message models.

---
+++ core/src/main/java/com/tatakae/admin/core/Message.java
@@ -0,0 +1,43 @@
+package com.tatakae.admin.core;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private final String id;
+    private final String type;
+    private final String data;
+    private final LocalDateTime date;
+
+    public Message(String id, String type, String data, LocalDateTime date) {
+        this.id = id;
+        this.type = type;
+        this.data = data;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id:'" + id + '\'' +
+                ", type:'" + type + '\'' +
+                ", data:'" + data + '\'' +
+                ", date:" + date +
+                '}';
+    }
+}
