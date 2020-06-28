e4f1775bb635e5af274903f2e250f9f3f8d184cf
NenetteGit <antonin.texier@gmail.com>
2020-06-29 00:13:00 +0200

    fix(core): Create an Exception if plugin name not found.

---
+++ core/src/main/java/com/tatakae/admin/core/Exceptions/PluginNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tatakae.admin.core.Exceptions;
+
+public class PluginNotFoundException extends Exception {
+    public PluginNotFoundException(String message) {
+        super(message);
+    }
+}
