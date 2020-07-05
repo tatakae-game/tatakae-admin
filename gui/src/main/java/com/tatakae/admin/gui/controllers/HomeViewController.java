8f3a024a0afa6a4774c3a6638611894b8c370d04
NenetteGit <antonin.texier@gmail.com>
2020-07-05 16:01:53 +0200

    feat(gui): Add and Remove button from Home menu.

--- gui/src/main/java/com/tatakae/admin/gui/controllers/HomeViewController.java
+++ gui/src/main/java/com/tatakae/admin/gui/controllers/HomeViewController.java
@@ -1,5 +1,9 @@
 package com.tatakae.admin.gui.controllers;
 
+import com.tatakae.admin.core.Exceptions.PluginNotFoundException;
+import com.tatakae.admin.core.PluginManager;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -19,10 +23,19 @@ public class HomeViewController {
     @FXML
     private VBox vBoxMenuContainer;
 
+    private PluginManager pluginManager;
+
     @FXML
     public void initialize() {
         try {
-            viewsContainer.getChildren().setAll((Node)FXMLLoader.load(getClass().getResource("/views/TicketsView.fxml")));
+            pluginManager = PluginManager.getInstance();
+
+            for(final var env : pluginManager.environmentsActive) {
+                addButtonInVBox(env.getPlugin().getName());
+            }
+
+            viewsContainer.getChildren()
+                    .setAll((Node)FXMLLoader.load(getClass().getResource("/views/TicketsView.fxml")));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -31,15 +44,13 @@ public class HomeViewController {
     @FXML
     public void loadPluginsView() {
         try {
-            viewsContainer.getChildren().setAll((Node)FXMLLoader.load(getClass().getResource("/views/PluginView.fxml")));
+            var loader = new FXMLLoader(getClass().getResource("/views/PluginView.fxml"));
+            viewsContainer.getChildren().setAll((Node) loader.load());
+            PluginViewController pluginViewController = loader.getController();
+            pluginViewController.setHomeController(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @FXML
-    public void loadErrorsView() {
-        System.out.println("Display errors view");
     }
 
     @FXML
@@ -51,19 +62,30 @@ public class HomeViewController {
         }
     }
 
-    @FXML
-    public void loadGroupsView() {
-        System.out.println("Display groups of permissions view");
-    }
-
     public void addButtonInVBox(final String name) {
         Button button = new Button(name);
+        button.setId(name);
         button.setStyle("-fx-background-color: #ca792b");
         button.setFont(new Font("system", 14));
         button.setTextFill(Color.WHITE);
         button.setMinWidth(178);
 
+        EventHandler<ActionEvent> event = e -> {
+            try {
+                final var env = pluginManager.getEnvironmentByPluginName(name);
+                viewsContainer.getChildren().setAll((Node)FXMLLoader.load(env.getPlugin().getView()));
+            } catch (PluginNotFoundException | IOException pluginNotFoundException) {
+                pluginNotFoundException.printStackTrace();
+            }
+        };
+
+        button.setOnAction(event);
+
         VBox.setMargin(button, new Insets(24, 0, 8, 0));
         vBoxMenuContainer.getChildren().add(button);
+    }
+
+    public void removeButtonFromVBox(final String name) {
+        vBoxMenuContainer.getChildren().removeIf(btn -> btn.getId().equals(name));
     }
 }
