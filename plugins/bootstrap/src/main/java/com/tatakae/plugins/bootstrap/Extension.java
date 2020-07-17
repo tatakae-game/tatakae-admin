992963d104457a9b1304d56526ef89c04edd6e08
NenetteGit <antonin.texier@gmail.com>
2020-07-17 16:34:06 +0200

    feat(core): Implement method startCLI().

--- plugins/bootstrap/src/main/java/com/tatakae/plugins/bootstrap/Extension.java
+++ plugins/bootstrap/src/main/java/com/tatakae/plugins/bootstrap/Extension.java
@@ -1,15 +1,27 @@
-package com.tatakae.plugins.boostrap;
-
-import java.net.URL;
-
 import com.tatakae.admin.core.Plugin;
 
 public class Extension implements Plugin {
+    public static String name = "Plugin 1";
+    public static String view = "view.fxml";
+    public static String description = "Here the description of Plugin 1";
+
     public void start() {
-        System.out.println("test");
+        System.out.println("Start Plugin 1");
     }
 
-    public URL getView() {
-        return getClass().getResource("/view.fxml");
+    public String getMainViewName() {
+        return view;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startCLI() {
+        System.out.println("Start CLI of plugin 1");
     }
 }
