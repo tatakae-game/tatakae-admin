d16fb390c312b35af196d03e2bbaae9d091c6385
Simon Halimi <halimisimon@gmail.com>
2020-07-21 11:51:25 +0200

    feat(): Add static app call

--- core/src/main/java/com/tatakae/admin/core/Config.java
+++ core/src/main/java/com/tatakae/admin/core/Config.java
@@ -1,5 +1,5 @@
 package com.tatakae.admin.core;
 
 public class Config {
-    public static final String url = "http://localhost:3000";
+    public static final String url = "https://api.tatakae.qtmsheep.com";
 }
